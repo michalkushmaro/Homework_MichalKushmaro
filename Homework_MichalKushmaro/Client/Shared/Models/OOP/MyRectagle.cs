531d5efa9cfc5f58be86d477aa6cf44d7eb55c6d
michalkushmaro <michalkushmaro2000@gmail.com>
2022-11-26 15:02:50 +0200

    מטלת OOP
    שיעורי בית מטלה 4

---
+++ Homework_MichalKushmaro/Client/Shared/Models/OOP/MyRectagle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Homework_MichalKushmaro.Client.Shared.Models.OOP
+{
+    public class MyRectagle
+    {
+
+        public string color { get; set; }
+        public double height { get; set; }
+        public double width { get; set; }
+
+public double Area()
+        {
+            double Area1 = height * width;
+            return Area1;
+        }
+
+        public double Perimeter()
+        {
+            double Perimeter1 = height + height + width + width;
+            return Perimeter1;
+
+
+        }
+
+    }
+    }
+
+
